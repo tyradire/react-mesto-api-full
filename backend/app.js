1040fdc3dab77318e0014fbfb97bbe31985bb0a2
Daniel Ugarov <tyradire@yandex.ru>
2021-11-05 15:09:36 +0300

    popit 5

--- backend/app.js
+++ backend/app.js
@@ -38,12 +38,7 @@ const allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  if (req.method === 'OPTIONS') {
-    res.send(200);
-  } else {
-    next();
-  }
+  next();
 };
 //творчество
 app.use(express.json());
