51d5239358c44a8a9c74de8e739cb4b60d8cf7ac
Daniel Ugarov <tyradire@yandex.ru>
2021-11-06 18:30:42 +0300

    here we go again

--- backend/app.js
+++ backend/app.js
@@ -39,6 +39,12 @@ app.use('*', cors(options));
 app.use(express.json());
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
